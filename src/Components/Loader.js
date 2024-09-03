1be644d0981d3a5212b69320323c43972a4e5ab9
Cabezlea <Leocabezas@icloud.com>
2024-09-03 16:43:48 -0500

    Added Blackhole

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,8 +1,34 @@
 // src/Components/Loader.js
 import React from 'react';
+import { Html, useProgress } from '@react-three/drei';
 
 const CanvasLoader = () => {
-    return <div>Loading 3D Model...</div>;
+    const { progress } = useProgress(); // Hook to get loading progress
+
+    return (
+        <Html
+            as="div"
+            center
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+            }}
+        >
+            <span className="canvas-loader"></span> {/* This can be styled via CSS */}
+            <p
+                style={{
+                    fontSize: 14,
+                    color: '#F1F1F1',
+                    fontWeight: 800,
+                    marginTop: 40,
+                }}
+            >
+                {progress.toFixed(2)}%
+            </p>
+        </Html>
+    );
 };
 
 export default CanvasLoader;
