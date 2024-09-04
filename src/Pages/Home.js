b9ce543c94a8e7a18108480eed1f4d98f9cf722e
Cabezlea <Leocabezas@icloud.com>
2024-09-04 16:56:00 -0500

    Edited home component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './Home.css';
 
+const produceSpans = (text) => {
+    return text.split("").map((char, index) => (
+        <span key={index} style={{ animationDelay: `${index * 0.1}s` }}>
+      {char === " " ? "\u00A0" : char}
+    </span>
+    ));
+};
 
 const Home = () => {
     return (
-            <div className="hero-content">
-                <h1>Leandro Cabezas</h1>
-                <p className="subtitle">Software Developer</p>
-                <p className="description">I love crafting captivating experiences for the digital world to savor.</p>
-            </div>
-
+        <div className="hero-content">
+            <h1>{produceSpans("LEANDRO CABEZAS")}</h1>
+            <p className="subtitle">Software Developer</p>
+            <p className="description">
+                I love crafting captivating experiences for the digital world to savor.
+            </p>
+        </div>
     );
 };
 
