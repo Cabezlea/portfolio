cd4c377b94fc1c1cb5ef4e621480b92ce1dc7f24
Cabezlea <Leocabezas@icloud.com>
2024-10-04 22:51:50 -0500

    Portfolio section finished

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -9,17 +9,20 @@ const Portfolio = () => {
         {
             name: "Vinum - AI-Powered Wine Recommendation",
             description: "Engineered and co-founded Vinum, a personalized wine discovery mobile app with React Native, Xcode, and Grok AI's API for enhanced wine suggestions.",
-            image: VinumImage
+            image: VinumImage,
+            link: "https://github.com/Cabezlea/Vinum"
         },
         {
             name: "Mercadona - Financial Data Processing",
             description: "Developed a Python-based system for financial transaction categorization using advanced regex algorithms, enhancing budget management capabilities.",
-            image: MercadonaImage
+            image: MercadonaImage,
+            link: "https://github.com/Cabezlea/Main-Projects/tree/main/Python%20-%20Git/ScriptPythonMercadona"
         },
         {
             name: "Music Player - Full Stack App Development",
             description: "Crafted a cross-platform music player in C++ with QT, focusing on low-latency audio data handling and efficient system architecture.",
-            image: MusicPlayerImage
+            image: MusicPlayerImage,
+            link: "https://github.com/Cabezlea/MusicPlayer"
         }
     ];
 
@@ -29,7 +32,9 @@ const Portfolio = () => {
             <div className="projects-container">
                 {projects.map((project, index) => (
                     <div key={index} className="project-card">
-                        <img src={project.image} alt={project.name} className="project-image" />
+                        <a href={project.link} target="_blank" rel="noopener noreferrer">
+                            <img src={project.image} alt={project.name} className="project-image" />
+                        </a>
                         <h3 className="project-name">{project.name}</h3>
                         <p className="project-description">{project.description}</p>
                     </div>
